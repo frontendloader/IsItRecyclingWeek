3d5b4b90123eef86e5313f06dc02fb29449f49bb
frontendloader <dvorachek@gmail.com>
2016-04-21 23:50:49 -0500

    first commit

--- IsItRecyclingWeek/ViewController.swift
+++ IsItRecyclingWeek/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let appletonURLBase = "http://appletonapi.appspot.com/garbagecollection"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addressFieldChanged(sender: UITextField) {
+        let addrStr: String = sender.text!
+        let strLen: Int = (addrStr.characters.count)
+        if(strLen > 4) {
+            //do callout
+            let urlStr = self.appletonURLBase + "?addr=" + "1928+S+LAWE+ST"
+            let calloutURL = NSURL(string: urlStr)
+            let task = NSURLSession.sharedSession().dataTaskWithURL(calloutURL!) {(data, response, error) in
+                var jsonDict: Array<NSDictionary>!
+                do {
+                    jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as? Array<NSDictionary>
+                } catch {
+                    print(error)
+                }
+                print(jsonDict)
+            }
+            task.resume()
+        }
+    }
 }
 
