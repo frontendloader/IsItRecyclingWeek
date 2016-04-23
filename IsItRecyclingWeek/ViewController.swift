185116b375a0bd1a27606bf7e5bf2bab341b2ca7
frontendloader <dvorachek@gmail.com>
2016-04-23 12:47:22 -0500

    break request out in to its own function, JSON deserialization, URL encoding on query params

--- IsItRecyclingWeek/ViewController.swift
+++ IsItRecyclingWeek/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var dateFor: NSDateFormatter = NSDateFormatter()
+
+    
+    class CollectionDate {
+        init(dict: NSDictionary) {
+            collectionType = dict["collectionType"] as! String
+            collectionDate = dict["collectionDate"] as! NSDate
+        }
+        var collectionType: String
+        var collectionDate: NSDate
+    }
+    
+    func deserializeResponse(JSONData: NSData) -> Array<NSDictionary> {
+        var jsonDict: Array<NSDictionary>!
+        do {
+            jsonDict = try NSJSONSerialization.JSONObjectWithData(JSONData, options: NSJSONReadingOptions()) as? Array<NSDictionary>
+            
+        } catch {
+            
+        }
+        return jsonDict
+    }
+    
+    func getCollectionDates(addrStr: String, callback: (result: String) -> Void) {
+        let urlStr = self.appletonURLBase + "?addr=" + addrStr.stringByAddingPercentEncodingWithAllowedCharacters(.
+            URLQueryAllowedCharacterSet())!
+        let calloutURL = NSURL(string: urlStr)
+        let task = NSURLSession.sharedSession().dataTaskWithURL(calloutURL!) {(data, response, error) in
+            var jsonDict: Array<NSDictionary>!
+            do {
+                jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as? Array<NSDictionary>
+            } catch {
+                print(error)
+            }
+            print(jsonDict)
+        }
+        task.resume()
+    }
+    
     let appletonURLBase = "http://appletonapi.appspot.com/garbagecollection"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.dateFor.dateFormat = "yyyy-MM-dd"
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,23 +62,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func savePress(sender: UIBarButtonItem) {
+        getCollectionDates(<#T##addrStr: String##String#>, callback: <#T##(result: String) -> Void#>)
+    
+    }
+    
+
+    
+    
     @IBAction func addressFieldChanged(sender: UITextField) {
         let addrStr: String = sender.text!
         let strLen: Int = (addrStr.characters.count)
         if(strLen > 4) {
-            //do callout
-            let urlStr = self.appletonURLBase + "?addr=" + "1928+S+LAWE+ST"
-            let calloutURL = NSURL(string: urlStr)
-            let task = NSURLSession.sharedSession().dataTaskWithURL(calloutURL!) {(data, response, error) in
-                var jsonDict: Array<NSDictionary>!
-                do {
-                    jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as? Array<NSDictionary>
-                } catch {
-                    print(error)
-                }
-                print(jsonDict)
-            }
-            task.resume()
+            /*do callout
+            */
         }
     }
 }
